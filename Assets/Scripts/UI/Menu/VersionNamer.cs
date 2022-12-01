c2d370c6f128a662e970cdac04a9ecc0e2ef0894
Alzter <alexsmudgy20@gmail.com>
2022-12-01 13:39:25 +1100

    Make the mod have a specific version number

--- Assets/Scripts/UI/Menu/VersionNamer.cs
+++ Assets/Scripts/UI/Menu/VersionNamer.cs
@@ -5,6 +5,6 @@ using TMPro;
 
 public class VersionNamer : MonoBehaviour {
     void Start() {
-        GetComponent<TMP_Text>().text = "v" + Application.version;
+        GetComponent<TMP_Text>().text = "Mod version: v" + Application.version + "\nGame version: v1.7.1.0-beta";
     }
 }
